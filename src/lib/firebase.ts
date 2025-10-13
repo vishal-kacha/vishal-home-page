3786481bb3620037fbbdb0d4e7451e7ef557cac0
vishal-kacha <vishaldineshbhaikacha@gmail.com>
2025-10-13 23:46:40 +0530

    firebase instanciated with config

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDwF5IkoNVJ5_IqzPcwkdgAQctSJOs9Z8A",
+  authDomain: "vishal-home-page-55596.firebaseapp.com",
+  projectId: "vishal-home-page-55596",
+  storageBucket: "vishal-home-page-55596.firebasestorage.app",
+  messagingSenderId: "903632524377",
+  appId: "1:903632524377:web:335a6093231a9a9f2d8d48",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
